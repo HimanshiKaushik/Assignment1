a006099582392e8589c5acc0e63f67382c6459f2
Himanshi Kaushik <120116004+HimanshiKaushik@users.noreply.github.com>
2022-12-17 18:17:44 +0530

    Add files via upload

---
+++ average of 3 numbers.py
@@ -0,0 +1,7 @@
+print("Average of 3 numbers is....")
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+sum = a+b+c
+avg = sum/3
+print("Average of 3 numbers is :",avg)
---
+++ greatest out of 3 numbers.py
@@ -0,0 +1,10 @@
+var1 = 16
+var2 = 58
+var3 = int(input())
+
+if var3>var2:
+    print("Greater")
+
+else:
+    print("Lesser")
+    
